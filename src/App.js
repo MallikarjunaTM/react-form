9c77c995dc3643e8984edb1316f640a00bfb7d28
MallikarjunaTM <mallikarjuna271@gmail.com>
2021-06-07 20:36:30 +0530

    simple hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [name,setName]=useState("");
+  const [income,setIncome]=useState("");
+
+  const handleChangeName=e=>{
+    const value=e.target.value;
+    const name=e.target.name;
+    console.log(value);
+    console.log(name)
+    if(name==='name'){
+      setName(value);
+    }
+
+    if(name==='income'){
+      setIncome(value)
+    }
+    
+  }
+
+  const handleSubmit=e=>{
+    console.log("state  ",name);
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <span>Enter a Name:</span>
+            <input type='text' name='name' value={name}  onChange={handleChangeName}/>
+          </div>
+          <div>
+            <span>Enter a Income:</span>
+            <select name='income' value={income} onChange={handleChangeName}>
+              <option value='High'>High</option>
+              <option value='Mid'>Mid</option>
+              <option value='Low'>Low</option>
+            </select>
+          </div>
+          <input type='submit' name='submit'/>
+        </form>
       </header>
     </div>
   );
