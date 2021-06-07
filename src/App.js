b989422122070e9e93683c17431131fc289b0551
MallikarjunaTM <mallikarjuna271@gmail.com>
2021-06-07 22:39:52 +0530

    useeffect how it works

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-
+import React, {useEffect, useState} from 'react';
+let born=false;
 function App() {
-  const [name,setName]=useState("");
-  const [income,setIncome]=useState("");
+  const [growth,setGrowth]=useState(0);
+  const [nirvana,setNirvana]=useState(false);
+  
+  useEffect(()=>{
+    if(born){
+      document.title='Nirvana attained'
+    }
+  },[nirvana])
 
-  const handleChangeName=e=>{
-    const value=e.target.value;
-    const name=e.target.name;
-    console.log(value);
-    console.log(name)
-    if(name==='name'){
-      setName(value);
+  useEffect(()=>{
+    console.log('born')
+  },[]);
+
+  useEffect(()=>{
+    if(born){
+    console.log('make mistake and learn');
+    }
+    else{
+      born=true;
     }
 
-    if(name==='income'){
-      setIncome(value)
+    if(growth>70){
+      setNirvana(true);
     }
-    
-  }
 
-  const handleSubmit=e=>{
-    console.log("state  ",name);
-    e.preventDefault();
-  }
+    return function cleanUp(){
+      console.log('cleanup mistakes');
+    }
+  })
 
+  const growthHandle =e=>{
+    setGrowth(growth+10);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <span>Enter a Name:</span>
-            <input type='text' name='name' value={name}  onChange={handleChangeName}/>
-          </div>
-          <div>
-            <span>Enter a Income:</span>
-            <select name='income' value={income} onChange={handleChangeName}>
-              <option value='High'>High</option>
-              <option value='Mid'>Mid</option>
-              <option value='Low'>Low</option>
-            </select>
-          </div>
-          <input type='submit' name='submit'/>
-        </form>
+      <h1>Use Effect</h1>
+      <h1>Growth:{growth}</h1>
+      <button onClick={growthHandle}>Learn and grow</button>      
       </header>
     </div>
   );
