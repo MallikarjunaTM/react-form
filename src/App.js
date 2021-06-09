ee1162129425d50903b0bf62c7f28477d2240f32
MallikarjunaTM <mallikarjuna271@gmail.com>
2021-06-09 14:21:51 +0530

    examples of useEffect, useMemo

--- src/App.js
+++ src/App.js
@@ -1,49 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState,useEffect,useMemo} from 'react';
+import Child from './component/Child';
+
+
+let initXY={
+  x:null,
+  y:null
+}
+
+let initialprofile={
+  public_repos:null,
+  followers:null
+}
+
 
 function App() {
-  const [name,setName]=useState("");
-  const [income,setIncome]=useState("");
 
-  const handleChangeName=e=>{
-    const value=e.target.value;
-    const name=e.target.name;
-    console.log(value);
-    console.log(name)
-    if(name==='name'){
-      setName(value);
-    }
+  const [time,setTime]=useState(Date);
+  const [xy,setXY]=useState(initXY);
+  const [profile,setProfile]=useState(initialprofile);
+  const [k,setK]=useState(0);
+  async function getProfile(){
+    const resp = await fetch('https://api.github.com/users/gitmil');
+    const response = await resp.json();
+    setProfile({
+      public_repos:response.public_repos,
+      followers:response.followers
+    })
 
-    if(name==='income'){
-      setIncome(value)
-    }
-    
   }
 
-  const handleSubmit=e=>{
-    console.log("state  ",name);
-    e.preventDefault();
+  useEffect(()=>{
+    getProfile()
+  },[])
+
+  useEffect(()=>{
+    let handle=setInterval(()=>{
+      setTime(Date)},1000)
+
+      return ()=>{
+        clearInterval(handle);
+      }
+  })
+
+  const mousemoveon = e =>{
+    setXY({
+      x:e.clientX,
+      y:e.clientY
+    })
   }
+  useEffect(()=>{
+    window.addEventListener('mousemove',mousemoveon)
+    return ()=>{
+      window.removeEventListener('mousemove',mousemoveon)
+    }
+  })
+
+  const onClickhandle=e=>{
+    setK(k+1);
+  }
+
+  const memoChild=useMemo(()=>{
+        return <Child></Child>
+  },[])
 
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <span>Enter a Name:</span>
-            <input type='text' name='name' value={name}  onChange={handleChangeName}/>
-          </div>
-          <div>
-            <span>Enter a Income:</span>
-            <select name='income' value={income} onChange={handleChangeName}>
-              <option value='High'>High</option>
-              <option value='Mid'>Mid</option>
-              <option value='Low'>Low</option>
-            </select>
-          </div>
-          <input type='submit' name='submit'/>
-        </form>
+        <h1>Current date & Time is:{time}</h1>
+        <h2>{`x:${xy.x}, y:${xy.y}`}</h2>
+        <h1>Profile:{`followers:${profile.followers}: public_repos:${profile.public_repos}`}</h1>
+        <h1>UseMemo</h1>
+        <h2>I:{k}</h2>
+        <button onClick={onClickhandle}>Increment I</button>
+        <Child></Child>
+        <h1>useMemo</h1>
+        {memoChild}
       </header>
     </div>
   );
---
+++ src/component/Child.js
@@ -0,0 +1,12 @@
+import React,{useEffect} from 'react';
+
+let renderCount=0;
+function Child(){
+    useEffect(()=>{
+        renderCount++;
+    })
+
+    return <div>renderCount:{renderCount}</div>
+}
+
+export default Child;
